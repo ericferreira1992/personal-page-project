d648499b20493812a6059d83e1989e8044571999
Eric <ericferreira1992@gmail.com>
2019-04-06 20:23:44 -0300

    Changes of improvements

--- src/app/about-me/about-me.js
+++
@@ -1,7 +0,0 @@
-export default class AboutMePage {
-    templateUrl = 'views/about-me.html';
-    element = null;
-
-    constructor() {
-    };
-}
--- src/index.js
+++
@@ -1,7 +0,0 @@
-export * from './app/router.js';
-export * from './app/main-page/main-page.js';
-export * from './app/about-me/about-me.js';
-export * from './app/my-skills/my-skills';
-export * from './app/my-works/my-works';
-export * from './app/talk-me/talk-me';
-import './assets/scss/style.scss';
